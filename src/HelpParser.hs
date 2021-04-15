c0c38d4158a904114b5610ac383a04f2c2e7a80c
yamato matsuoka <yamato.matsuoka@tetradresearch.com>
2021-04-15 17:33:48 -0400

    Improve option line parsing for multiple names and arguments

--- src/HelpParser.hs
+++ src/HelpParser.hs
@@ -1,26 +1,34 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module HelpParser where
 
 import Control.Applicative ((<|>))
 import Text.ParserCombinators.ReadP
 
-data Option = Option
-  { _optionName :: OptionName,
-    _optionDesc :: String,
-    _optionArg :: Maybe String
+data Opt = Opt
+  { _name :: OptName,
+    _desc :: String,
+    _arg :: Maybe String
   }
 
-data OptionName = OptionName
-  { _optionNameBase :: String,
-    _optionNameType :: OptionNameType
+data OptName = OptName
+  { _raw :: String,
+    _type :: OptNameType
   }
+  deriving (Eq)
 
-data OptionNameType = LONG | SHORT | OLD deriving (Eq, Show)
+instance Show OptName where
+  show (OptName raw t) = raw
+
+data OptNameType = LongType | ShortType | OldType deriving (Eq, Show)
 
 allDigits = "0123456789"
 
 allAlphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 alphanum = allAlphabets ++ allDigits
+
+extraSymbols = "+-_?"
 
 dash :: ReadP Char
 dash = satisfy (== '-')
@@ -35,7 +43,7 @@ isAscii :: Char -> Bool
 isAscii c = c `elem` alphanum
 
 isAllowedCharInName :: Char -> Bool
-isAllowedCharInName c = c `elem` ('-' : '_' : alphanum)
+isAllowedCharInName c = c `elem` (alphanum ++ extraSymbols)
 
 newline :: ReadP Char
 newline = char '\n'
@@ -43,56 +51,79 @@ newline = char '\n'
 word :: ReadP String
 word = munch1 (`notElem` " \t\n")
 
+argWord :: ReadP String
+argWord = munch1 (\c -> c `elem` (alphanum ++ "[]<>{},"))
+
 sentence :: ReadP String
 sentence = do
   xs <- sepBy word (munch1 (== ' '))
   return (unwords xs)
 
-optionBase :: ReadP String
-optionBase = do
+optStem :: ReadP String
+optStem = do
   head <- ascii
   tail <- munch isAllowedCharInName
   return (head : tail)
 
-longOptionName :: ReadP String
-longOptionName = do
+longOptName :: ReadP OptName
+longOptName = do
   _ <- count 2 dash
-  name <- optionBase
-  return ("--" ++ name)
+  name <- optStem
+  let res = OptName ("--" ++ name) LongType
+  return res
 
-shortOptionName :: ReadP String
-shortOptionName = do
+shortOptName :: ReadP OptName
+shortOptName = do
   _ <- dash
   c <- ascii
-  return ('-' : c : "")
+  let res = OptName ('-' : c : "") ShortType
+  return res
 
-oldOptionName :: ReadP String
-oldOptionName = do
+oldOptName :: ReadP OptName
+oldOptName = do
   _ <- dash
-  name <- optionBase
-  return ('-' : name)
+  head <- ascii
+  tail <- optStem
+  let res = OptName ('-' : head : tail) OldType
+  return res
 
-optionArg :: ReadP String
-optionArg = do
+optName :: ReadP OptName
+optName = longOptName <|> (oldOptName <++ shortOptName)
+
+altOptionName :: ReadP OptName
+altOptionName = do
+  char ','
+  optional singleSpace
+  optName
+
+optNames :: ReadP [OptName]
+optNames = do
+  name <- optName
+  maybeAlt <- fmap Just altOptionName <++ pure Nothing
+  case maybeAlt of
+    Just alt -> return [name, alt]
+    Nothing -> return [name]
+
+optArgs :: ReadP String
+optArgs = do
   singleSpace <|> char '='
-  arg <- munch1 isAscii
-  singleSpace
-  return arg
+  args <- sepBy1 argWord singleSpace
+  return (unwords args)
 
 skip :: ReadP a -> ReadP ()
-skip a = do
-  a
-  return ()
+skip a = a >> return ()
 
-optionItem :: ReadP (String, String, Maybe String)
-optionItem = do
+optItem :: ReadP ([OptName], String, Maybe String)
+optItem = do
   skipSpaces
-  name <- longOptionName <|> (oldOptionName <++ shortOptionName)
-  arg <- fmap Just optionArg <++ pure Nothing
+  names <- optNames
+  args <- fmap Just optArgs <++ pure Nothing
   skipSpaces
   char ':' <++ pure 'x' -- the latter is just a placeholder; consume the former if applies.
+  char '\n' <++ pure 'x'
   skipSpaces
   desc <- sentence
   skipSpaces
   skip newline <++ eof
-  return (name, desc, arg)
+  return (names, desc, args)
+
