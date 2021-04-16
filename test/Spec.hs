ceb14ef854ce27b95487185e15960413cc586b4e
yamaton <yamaton@gmail.com>
2021-04-16 19:07:59 -0400

    Add more unit tests with helper functions

--- test/Spec.hs
+++ test/Spec.hs
@@ -19,8 +19,50 @@ unitTests =
       testCase "optName (old)" $
         readP_to_S optName "-azvhP" @?= [(OptName "-azvhP" OldType, "")],
       testCase "optName (double dash alone)" $
-        readP_to_S optName "-- " @?= [(OptName "--" DoubleDashAlone, "")]
-
+        readP_to_S optName "-- " @?= [(OptName "--" DoubleDashAlone, "")],
+      test_optItem "--help   baba keke" (["--help"], "", "baba keke"),
+      test_optItem "-h,--help   baba keke" (["-h", "--help"], "", "baba keke"),
+      test_optItem "-h, --help   baba" (["-h", "--help"], "", "baba"),
+      test_optItem "-o ARG   baba" (["-o"], "ARG", "baba"),
+      test_optItem "-o,--out ARG   baba" (["-o", "--out"], "ARG", "baba"),
+      test_optItem "-o,--out=ARG   baba" (["-o", "--out"], "ARG", "baba"),
+      test_optItem "-o,--out ARG: baba" (["-o", "--out"], "ARG", "baba"),
+      test_optItem "-o,--out ARG\n   baba" (["-o", "--out"], "ARG", "baba"),
+      test_optItem "-o,--out ARG:  baba" (["-o", "--out"], "ARG", "baba"),
+      test_optItem "-o ARG   baba" (["-o"], "ARG", "baba"),
+      test_optItem "-o=ARG   baba" (["-o"], "ARG", "baba"),
+      test_optItem "--out=ARG[,ARG2] baba" (["--out"], "ARG[,ARG2]", "baba"),
+      test_optItem "--out, -o ARG    baba" (["--out", "-o"], "ARG", "baba"),
+      test_optItem "-out: baba" (["-out"], "", "baba"),
+      test_optItem "-out:\n baba" (["-out"], "", "baba"),
+      -- examples in the wild
+      test_optItem
+        "  -E, --show-ends          display $ at end of each line"
+        (["-E", "--show-ends"], "", "display $ at end of each line"),
+      test_optItem
+        " -h --help       Print this help file and exit"
+        (["-h", "--help"], "", "Print this help file and exit"),
+      test_optItem
+        "--min_length    Sets an artificial lower limit"
+        (["--min_length"], "", "Sets an artificial lower limit"),
+      test_optItem
+        " -O INT[,INT] gap open penalty [4,24]"
+        (["-O"], "INT[,INT]", "gap open penalty [4,24]"),
+      test_optItem
+        "-w, --line-width int                  line width"
+        (["-w", "--line-width"], "int", "line width"),
+      -- fails on these cases now...
+      --    conda install --help
+      test_optItem
+        " -p PATH, --prefix PATH\n     Full path to environment location"
+        (["-p", "--prefix"], "PATH", "Full path to environment location"),
+      --    minimap2 --help
+      test_optItem
+        "--cs[=STR]   output the cs tag; STR is 'short' (if absent) or 'long' [none]"
+        (["--cs"], "[STR]", "output the cs tag; STR is 'short' (if absent) or 'long' [none]")
+        -- Also I have no idea how to handle case like following..
+        --   stack --help
+        --     "--[no-]dump-logs         Enable/disable dump the build output logs"
     ]
 
 propertyTests =
@@ -34,7 +76,7 @@ propertyTests =
 prop_longOpt :: Property
 prop_longOpt =
   property $ do
-    s <- forAll (Gen.string (Range.linear 1 10) Gen.alphaNum)
+    s <- forAll (Gen.string (Range.constant 1 10) Gen.alphaNum)
     let ddashed = "--" ++ s
     readP_to_S optName ddashed === [(OptName ddashed LongType, "")]
 
@@ -48,6 +90,20 @@ prop_shortOpt =
 prop_oldOpt :: Property
 prop_oldOpt =
   property $ do
-    s <- forAll (Gen.string (Range.linear 2 10) Gen.alphaNum)
+    s <- forAll (Gen.string (Range.constant 2 10) Gen.alphaNum)
     let dashed = "-" ++ s
     readP_to_S optName dashed === [(OptName dashed OldType, "")]
+
+makeOpt :: [String] -> String -> String -> Opt
+makeOpt names = Opt (map getOptName names)
+  where
+    getOptName s = case readP_to_S optName s of
+      [(optname, _)] -> optname
+      _ -> undefined
+
+test_optItem :: String -> ([String], String, String) -> TestTree
+test_optItem s (names, args, desc) =
+  testCase s $
+    readP_to_S optItem s @?= [(opt, "")]
+  where
+    opt = makeOpt names args desc
