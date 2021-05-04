4369457024c93ce3859451729f2a2ecdf7e9ec08
yamaton <yamaton@gmail.com>
2021-05-03 21:26:00 -0400

    Add CLI interface with optparse-applicative

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,63 @@
 module Main where
 
 import qualified Data.List as List
+import Data.Semigroup ((<>))
+import GenBashCompletions
+import GenFishCompletions
+import GenZshCompletions
 import HelpParser
+import Options.Applicative
 import Subcommand
-import System.Environment (getArgs)
+
+data Config = Config
+  { input :: String,
+    shell :: String,
+    subcommand :: Bool
+  }
+
+config :: Parser Config
+config =
+  Config
+    <$> strOption
+      ( long "ipnut"
+          <> short 'i'
+          <> metavar "<FILE>"
+          <> help "Select a text file to parse."
+      )
+    <*> strOption
+      ( long "shell"
+          <> short 's'
+          <> metavar "<SHELL>"
+          <> showDefault
+          <> value "none"
+          <> help "Select shell for a completions script (bash/zsh/fish/none)"
+      )
+    <*> switch
+      ( long "subcommand"
+          <> short 'c'
+          <> help "Enable subcommand parsing"
+      )
 
 main :: IO ()
-main = do
-  files <- getArgs
-  content <- readFile (head files)
-  let xs = parseMany content
-  putStr $ List.intercalate "\n" $ map show xs
+main = run =<< execParser opts
+  where
+    opts =
+      info
+        (config <**> helper)
+        ( fullDesc
+            <> progDesc "Parse manpage/help and generate shell completion script"
+            <> header "shellcomp"
+        )
 
+run :: Config -> IO ()
+run (Config f shell _) = do
+  content <- readFile f
+  let opts = parseMany content
+  let cmd = "nanachi"
+  putStr $ gen shell cmd opts
 
-  -- [TODO] Introduce subcommand to switch?
-  -- let xs = parseSubcommand content
-  -- putStr $ List.intercalate "\n" $ map show xs
+gen :: String -> String -> [Opt] -> String
+gen "fish" = genFishScript
+gen "zsh" = genZshScript
+gen "bash" = genBashScript
+gen _ = \cmd opts -> List.intercalate "\n" $ map show opts
