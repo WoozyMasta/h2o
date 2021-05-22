fca2b2a61eefe6c4c8ffea462e325dbb150a63c9
yamaton <yamaton@gmail.com>
2021-05-22 00:40:01 -0400

    Change CLI to take subcommand or command or file as input
    Here subcommand must be given as hyphen-separated like
    `h2o --subcommand stack-build`

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import Data.List.Extra (stripInfix)
+import qualified Data.Maybe as Maybe
 import Debug.Trace (trace)
 import GenBashCompletions (genBashScript)
 import GenFishCompletions
@@ -14,15 +16,15 @@ import Options.Applicative
 import Subcommand (Subcommand (..), parseSubcommand)
 import System.FilePath (takeBaseName)
 import System.Process (readProcess)
-import Utils (convertTabsToSpaces, debugShow)
+import Utils (convertTabsToSpaces)
 
 data Input
-  = CommandInput String
+  = SubcommandInput String String
+  | CommandInput String
   | FileInput FilePath
 
 data Config = Config
   { _input :: Input,
-    _subname :: String,
     _shell :: String,
     _isParsingSubcommand :: Bool,
     _isConvertingTabsToSpaces :: Bool,
@@ -30,45 +32,49 @@ data Config = Config
     _isPreprocessOnly :: Bool
   }
 
+subcommandInput :: Parser Input
+subcommandInput =
+  uncurry SubcommandInput . Maybe.fromJust . stripInfix "-"
+    <$> strOption
+      ( long "subcommand"
+          <> short 's'
+          <> metavar "<STRING>-<STRING>"
+          <> help "Specify command-subcommand name to parse"
+      )
+
 commandInput :: Parser Input
 commandInput =
   CommandInput
     <$> strOption
       ( long "command"
           <> short 'c'
-          <> metavar "<COMMAND>"
-          <> help "Select a text file to parse."
+          <> metavar "<STRING>"
+          <> help "Specify command to parse."
       )
 
 fileInput :: Parser Input
 fileInput =
   FileInput
     <$> strOption
-      ( long "input"
-          <> short 'i'
+      ( long "file"
+          <> short 'f'
           <> metavar "<FILE>"
-          <> help "Select a text file to parse."
+          <> help "Select a text file to parse"
       )
 
 inputP :: Parser Input
-inputP = commandInput <|> fileInput
+inputP = subcommandInput <|> commandInput <|> fileInput
 
 config :: Parser Config
 config =
   Config
     <$> inputP
     <*> strOption
-      ( long "subname"
-          <> metavar "<SUBCOMMAND>"
-          <> value ""
-          <> help "Specify subcommand name"
-      )
-    <*> strOption
       ( long "shell"
-          <> metavar "<SHELL>"
+          <> metavar "{bash|zsh|fish|none}"
           <> showDefault
           <> value "none"
-          <> help "Select shell for a completions script (bash/zsh/fish/none)"
+          <> help "Select shell for a completions script (bash|zsh|fish|none)"
       )
     <*> switch
       ( long "parse-subcommand"
@@ -79,8 +85,8 @@ config =
           <> help "Convert tabs to spaces"
       )
     <*> switch
-      ( long "list-subcommands (experimental)"
-          <> help "Convert tabs to spaces"
+      ( long "list-subcommands"
+          <> help "List subcommands (experimental)"
       )
     <*> switch
       ( long "debug"
@@ -98,30 +104,51 @@ main = execParser opts >>= run
         )
 
 run :: Config -> IO ()
-run (Config input subname shell isParsingSubcommand isConvertingTabsToSpaces isListingSubcommands isPreprocessOnly) = do
+run (Config input shell isParsingSubcommand isConvertingTabsToSpaces isListingSubcommands isPreprocessOnly) = do
   content <- case input of
-    FileInput f -> readFile f
+    SubcommandInput name subname -> getHelpSub name subname
     CommandInput name -> getHelp name
+    FileInput f -> readFile f
   let subcommands = parseSubcommand content
   let opts = parseMany content
   let s
-        | isConvertingTabsToSpaces = trace "[main] Converting tags to spaces...\n" $ convertTabsToSpaces 8 content
-        | isListingSubcommands = trace "[main] Listing subcommands...\n" $ unlines (map _cmd subcommands)
-        | isParsingSubcommand && isPreprocessOnly = trace "[main] processing subcommands only" $ genSubcommandScript cmd subcommands
-        | isParsingSubcommand && null subname = trace "[main] processing subcommands + top-level options" $ genSubcommandScript cmd subcommands ++ "\n\n\n" ++ genOptScript shell cmd opts
-        | isParsingSubcommand = trace "[main] processing subcommands + subcommand-level options" $ genSubcommandScript cmd subcommands ++ "\n\n\n" ++ genSubcommandOptScript cmd subname opts
-        | isPreprocessOnly = trace "[main] processing (option+arg, description) splitting only" $ formatStringPairs $ preprocessAll content
-        | null subname = trace "[main] processing top-level options" $ genOptScript shell cmd opts
-        | otherwise = trace "[main] processing subcommand-level options" $ genSubcommandOptScript cmd subname opts
+        | isConvertingTabsToSpaces =
+          trace "[main] Converting tags to spaces...\n" $
+            convertTabsToSpaces 8 content
+        | isListingSubcommands =
+          trace "[main] Listing subcommands...\n" $ unlines (map _cmd subcommands)
+        | isParsingSubcommand && isPreprocessOnly =
+          trace "[main] processing subcommands only" $
+            genSubcommandScript cmd subcommands
+        | isPreprocessOnly =
+          trace "[main] processing (option+arg, description) splitting only" $
+            formatStringPairs $ preprocessAll content
+        | otherwise =
+          case input of
+            SubcommandInput _ subname ->
+              if isParsingSubcommand
+                then
+                  trace "[main] processing subcommands + subcommand-level options" $
+                    genSubcommandScript cmd subcommands ++ "\n\n\n" ++ genSubcommandOptScript cmd subname opts
+                else
+                  trace "[main] processing subcommand-level options" $
+                    genSubcommandOptScript cmd subname opts
+            _ ->
+              trace "[main] processing subcommands + top-level options" $
+                genSubcommandScript cmd subcommands ++ "\n\n\n" ++ genOptScript shell cmd opts
   putStr s
   where
     formatStringPairs = unlines . map (\(a, b) -> unlines [a, b])
-    cmd = debugShow "subname: " subname $ case input of
+    cmd = case input of
+      SubcommandInput name _ -> name
       CommandInput name -> name
       FileInput f -> takeBaseName f
 
 getHelp :: String -> IO String
 getHelp cmd = readProcess cmd ["--help"] ""
+
+getHelpSub :: String -> String -> IO String
+getHelpSub cmd subcmd = readProcess cmd [subcmd, "--help"] ""
 
 genOptScript :: String -> String -> [Opt] -> String
 genOptScript "fish" = genFishScript
