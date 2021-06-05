2d1caf97eabb53733d577c2b16387f3cbeac9074
yamaton <yamaton@gmail.com>
2021-06-05 00:57:57 -0400

    Fix subcommand detection

--- src/Subcommand.hs
+++ src/Subcommand.hs
@@ -2,11 +2,9 @@ module Subcommand where
 
 import Control.Monad (liftM2)
 import qualified Data.Maybe as Maybe
-import Debug.Trace (trace)
-import HelpParser (newline, optWord, singleSpace, skip, word)
+import HelpParser (isAlphanum, newline, singleSpace, skip, word)
 import Layout (getDescriptionOffset)
 import Text.ParserCombinators.ReadP
-    ( ReadP, (<++), eof, munch, readP_to_S, sepBy1, skipSpaces )
 import Type (Subcommand (..))
 import Utils (convertTabsToSpaces, debugMsg, getMostFrequent, startsWithChar)
 
@@ -25,30 +23,40 @@ firstTwoWordsLoc line = (firstLoc, secondLoc)
     firstLoc = if null w then -1 else length spaces
     secondLoc = if null rest2 then -1 else firstLoc + length w + length midSpaces
 
-getLayoutMaybe :: [String] -> Maybe Layout
-getLayoutMaybe xs = liftM2 (,) first second
+getLayoutMaybe :: [String] -> Int -> Maybe Layout
+getLayoutMaybe xs offset = liftM2 (,) first second
   where
-    pairs = debugMsg "first two word locations:" $ filter (\(a, b) -> a > 0 && b >= a + 6) $ map firstTwoWordsLoc xs
+    pairs =
+      debugMsg "first two word locations:" $
+        filter (\(a, b) -> a > 0 && b >= a + 6 && a < offset) $ map firstTwoWordsLoc xs
     second = getMostFrequent [b | (_, b) <- pairs]
     first = getMostFrequent [a | (a, _) <- pairs, a < Maybe.fromMaybe 50 second]
 
 getAlignedLines :: String -> [String]
-getAlignedLines s = case (layoutMay, offsetMay) of
-  (Just lay, Just offset) ->
-    if offset <= fst lay
-      then trace "[subcommand] discard layout because of description offset" []
-      else filter (\line -> firstTwoWordsLoc line == lay) xs
-  (Just lay, Nothing) -> filter (\line -> firstTwoWordsLoc line == lay) xs
+getAlignedLines s = case layoutMay of
+  Just lay -> filter (\line -> firstTwoWordsLoc line == lay) xs
   _ -> []
   where
-    xs = lines s
-    layoutMay = debugMsg "subcommand layout :" $ getLayoutMaybe xs
     offsetMay = getDescriptionOffset s
+    offset = debugMsg "subcommand offset :" $ Maybe.fromMaybe 50 offsetMay
+    xs = lines $ removeOptionLines s
+    layoutMay = debugMsg "subcommand layout :" $ getLayoutMaybe xs offset
+
+lowercase :: String
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+
+subcommandWord :: ReadP String
+subcommandWord = do
+  x <- satisfy $ \c -> c `elem` lowercase
+  xs <- munch isAlphanum
+  -- For example docker run --help has "--docker*"
+  _ <- char '*' <++ pure '*'
+  return (x : xs)
 
 subcommand :: ReadP Subcommand
 subcommand = do
   skipSpaces
-  cmd <- optWord
+  cmd <- subcommandWord
   skipSpaces
   ss <- sepBy1 word singleSpace
   skip (munch (`elem` " \t"))
@@ -70,6 +78,5 @@ parseSubcommand :: String -> [Subcommand]
 parseSubcommand content = results
   where
     s = convertTabsToSpaces 8 content
-    s' = removeOptionLines s
-    xs = getAlignedLines s'
+    xs = getAlignedLines s
     results = (map (fst . last) . filter (not . null)) [readP_to_S subcommand x | x <- xs]
